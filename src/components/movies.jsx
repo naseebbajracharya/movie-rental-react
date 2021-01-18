3b9e98dc3f4b2409aa5ba1e56578746c58bbbbc9
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-01-18 12:46:00 +0545

    fake movie data displayed into movies component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import {getMovies} from '../services/fakeMovieService';
 
 class Movies extends Component {
-    state = {}
+    state = {
+        movies: getMovies()
+    };
+
     render() { 
-        return (<h2>Movies Component</h2>);
+        return <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Available Stock</th>
+                    <th>Rate</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie => (
+                    <tr>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                </tr>
+                ))}
+                
+            </tbody>
+        </table>
     }
 }
  
