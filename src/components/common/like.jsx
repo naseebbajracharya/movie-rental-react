94b672fc4b1f45862e1ce530d0f5ea6b301f3434
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-01-26 20:58:47 +0545

    converting class of like.jsx into functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-
-    render() { 
-        let classes = "fa fa-thumbs-up";
-        if (!this.props.liked) classes += "fa fa-thumbs-o-up";
-        return ( <i onClick={this.props.onClick} style={{cursor:'pointer'}} className={classes} aria-hidden="true"></i> );
-    }
+const Like = (props) => {
+    let classes = "fa fa-thumbs-up";
+        if (!props.liked) classes += "fa fa-thumbs-o-up";
+        return ( <i onClick={props.onClick} style={{cursor:'pointer'}} className={classes} aria-hidden="true"></i> );
 }
  
 export default Like;
