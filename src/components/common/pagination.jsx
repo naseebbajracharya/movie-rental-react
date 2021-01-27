99a529637fefec83d3bda95e4c3f9e60ddf14fef
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-01-27 17:17:05 +0545

    works on pagination 2

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import _ from 'lodash';
 
 const Pagination = (props) => {
-    return null;
+
+    const {itemCount, pageSize} = props;
+
+    const pagesCount = itemCount/pageSize;
+
+    return <nav>
+        <ul className="pagination justify-content-center">
+            <li className="page-item">
+                <a onClick="" className="page-link">1</a>
+            </li>
+        </ul>
+    </nav>;
 }
  
 export default Pagination;
