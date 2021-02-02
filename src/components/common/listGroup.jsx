e004ab463ffae37a872073389b3bf9a75ca5d86b
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-02 22:37:48 +0545

    works on onclick event genre items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 const ListGroup = (props) => {
-    const {items, textProperty, valueProperty} = props;
+    const {items, textProperty, valueProperty, onItemSelect} = props;
         return ( 
             <ul className="list-group">
-                {items.map(item => <li key={item[valueProperty]} className="list-group-item list-group-item-success">
+                {items.map(item => <li onClick={() => onItemSelect(item)} key={item[valueProperty]} className="list-group-item list-group-item-success">
                     {item[textProperty]}
                     </li>)} 
             </ul>
