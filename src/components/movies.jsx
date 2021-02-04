42db6eed36e180673efc2fd3e821921e27ab20c5
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-04 20:27:54 +0545

    moving responsibility;sorting and fixing console warnings

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -47,15 +47,7 @@ class Movies extends Component {
         this.setState({selectedGenre: genre, currentPage: 1});
     }
 
-    handleSort = path => {
-        const sortColumn = {...this.state.sortColumn};
-        if(sortColumn.path === path)
-        sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
-
-        else {
-            sortColumn.path = path;
-            sortColumn.order = 'asc';
-        }
+    handleSort = sortColumn => {
         this.setState({sortColumn});
     }
 
@@ -82,7 +74,7 @@ class Movies extends Component {
             <div className="col">
             <h5>Showing <span class="badge badge-info">{filtered.length}</span> movies in the database.</h5>
 
-            <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} onSort={this.handleSort}/>
+            <MoviesTable movies={movies} sortColumn={sortColumn} onLike={this.handleLike} onDelete={this.handleDelete} onSort={this.handleSort}/>
         
         <Pagination itemsCount={filtered.length} 
                     pageSize={pageSize} 
