73c71e31e442d167e3b047681fbd5c7fb3d88b20
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-06 20:53:40 +0545

    works on cell content 2

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -10,8 +10,8 @@ class MoviesTable extends Component {
         {path: 'genre.name', label:'Genre'},
         {path: 'numberInStock', label:'Available Stock'},
         {path: 'dailyRentalRate', label:'Rate'},
-        {key: 'like'},
-        {key: 'delete'},
+        {key: 'like', content: <Like liked={movie.liked} onClick={() => onLike(movie)}/>},
+        {key: 'delete', content: <button onClick={() => onDelete(movie)} className="btn btn-danger btn-sm"><i class="fa fa-trash"> </i> Remove</button>},
     ];
  
     render() { 
@@ -22,19 +22,7 @@ class MoviesTable extends Component {
                 <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort}/>
 
                 <TableBody columns={this.columns} data={movies}/>
-                {/* <tbody>
-                    {movies.map(movie => (
-                        <tr key={movie._id}>
-                        <td>{movie.title}</td>
-                        <td>{movie.genre.name}</td>
-                        <td>{movie.numberInStock}</td>
-                        <td>{movie.dailyRentalRate}</td>
-                        <td><Like liked={movie.liked} onClick={() => onLike(movie)}/></td>
-                        <td><button onClick={() => onDelete(movie)} className="btn btn-danger btn-sm"><i class="fa fa-trash"> </i> Remove</button></td>
-                    </tr>
-                    ))}
-                    
-                </tbody> */}
+                
             </table>
          );   
     }
