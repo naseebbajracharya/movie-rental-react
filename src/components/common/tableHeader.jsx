95606a5f1dc2b9325eb3d27c1d6db4abac8fb198
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-07 21:14:15 +0545

    works on asc sort icons

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -16,6 +16,11 @@ class TableHeader extends Component {
         renderSortIcon = column => {
             if (column.path !== this.props.sortColumn.path)
             return null;
+
+            //rendering diff icons in terms of asc order
+            if (this.props.sortColumn.order === 'asc') {
+                return <i className="fa-sort-alpha-asc"></i>
+            }
         };
 
 
