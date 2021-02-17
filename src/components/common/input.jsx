8a156b0e8cda0cdf47c37c9af648f0c23ad5190c
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-02-17 13:53:16 +0545

    extraction of helper render method 4

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,10 +1,11 @@
+import { rest } from 'lodash';
 import React from 'react';
 
-const Input = ({name, label, value, onChange, type, error}) => {
+const Input = ({name, label, error, ...rest}) => {
     return ( 
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
-            <input value={value} onChange={onChange} name={name} id={name} type={type} className="form-control"/>
+            <input {...rest} name={name} id={name} className="form-control"/>
             {error && <div className="alert alert-danger alert-dismissible fade show">
                 {error} 
             </div>}
